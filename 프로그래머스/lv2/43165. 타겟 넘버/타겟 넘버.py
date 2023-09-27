f96cdef3e1f64e8c5e0a8b4a640c943d2b2ea4e8
yencoding <79498424+yeni28@users.noreply.github.com>
2023-09-27 13:41:49 +0900

    [level 2] Title: 타겟 넘버, Time: 2.81 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,20 +1,15 @@
 ans = 0
-
 def solution(numbers, target):
-    M = len(numbers)
-    nsum = 0
-    def dfs(n, nsum):
+    result = 0
+    def dfs(n, result):
         global ans
-        #리스트의 값을 모두 사용했나요?
-        if n == M:
-            #타겟값과 sum값이 같다면
-            if nsum == target:
+        if n == len(numbers):
+            if result == target:
                 ans += 1
-                return
-        else:
-            dfs(n + 1, nsum +numbers[n])
-            dfs(n + 1, nsum- numbers[n])
+            return
+        dfs(n+1, result + numbers[n])
+        dfs(n+1, result - numbers[n])
+    
     dfs(0,0)
-
     answer = ans
     return answer

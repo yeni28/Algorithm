d8337979792f30c8d9b54c2317a70b9357b11f57
yencoding <79498424+yeni28@users.noreply.github.com>
2023-09-28 16:24:58 +0900

    [level 2] Title: 타겟 넘버, Time: 5.23 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,21 @@
 ans = 0
 def solution(numbers, target):
-    result = 0
-    def dfs(n, result):
+    # nubers는 리스트, 타겟은 int값
+    # 뽑는 숫자의 수가 정해져있으므로 백트래킹을 사용해보자.
+    # 뽑아서 nsum이 target이 되면 그 값을 global 변수에 담아 더해준다.
+    def dfs(n, nsum):
         global ans
+        # 숫자를 다 뽑았으면
         if n == len(numbers):
-            if result == target:
+            # 그 합의 값이 target과 같으면
+            if nsum == target:
                 ans += 1
-            return
-        dfs(n+1, result + numbers[n])
-        dfs(n+1, result - numbers[n])
-    
+                return
+        else:
+            dfs(n+1, nsum + numbers[n])
+            dfs(n+1, nsum - numbers[n])
+        
     dfs(0,0)
     answer = ans
+    
     return answer

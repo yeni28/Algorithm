a300f88b95c801c56cb63f9c0c0f07374e90ab4c
yencoding <79498424+yeni28@users.noreply.github.com>
2023-10-04 20:14:00 +0900

    [level 2] Title: 큰 수 만들기, Time: ., Memory: undefined -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,21 +1,25 @@
+max_num = 0
+
 def solution(number, k):
-    stack = []
+    num = ''
+    N = len(number)
+    a = N-k
+    number = list(number)
+    def dfs(n,num,number):
+        global max_num
+        # 수의 조합을 다 썼나요?
+        if n == a:
+            if int(num) >= max_num:
+                max_num = int(num)
+            return
 
-    for digit in number:
-        # 스택에 요소가 있고, 스택의 맨 위 요소가 현재 digit보다 작고, 아직 제거해야 할 숫자가 남아있다면
-        while stack and stack[-1] < digit and k > 0:
-            stack.pop()  # 스택의 맨 위 요소를 제거하고 k를 1 감소
-            k -= 1
-        stack.append(digit)
-
-    # 만약 k개의 숫자를 제거하지 못한 경우, 뒤에서부터 남은 k개를 제거
-    stack = stack[:-k] if k > 0 else stack
-
-    answer = ''.join(stack)
+        else:
+            for i in range(N):
+                #중복 제거
+                number.pop(number[i])
+                dfs(n+1,num + number[i],number)
+                
+    dfs(0,num,number)
+        
+    answer = max_num
     return answer
-
-# 예제 테스트
-number = "1924"
-k = 2
-result = solution(number, k)
-print(result)  # 출력: "94"

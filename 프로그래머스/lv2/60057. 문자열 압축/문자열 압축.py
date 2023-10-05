550826c8b597169a961acc8c0b71c6658d8d2462
yencoding <79498424+yeni28@users.noreply.github.com>
2023-10-05 13:18:10 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,23 +1,31 @@
 def solution(s):
     N = len(s)
     result = []
-    if N ==1:
-        return 1
-    for i in range(1, N+1):
-        b = ''
+    #문자열을 i부터 N단위 까지 자를 것입니다.
+    for i in range(1,N+1):
+        # 압축된 것을 반환할 문자열
+        sentence = ''
+        # 중복 개수를 체크할 숫자
         cnt = 1
+        #미리 자른 문자열
         tmp = s[:i]
-        
-        #i부터 끝까지 i 단위로 나누어 탐색
-        for j in range(i, N+i, i):
-            if tmp == s[j:i+j]:
-                cnt+=1
+        for j in range(i,N+i,i):
+            # 만약 tmp랑 같으면(반복되는 문자열이면)
+            if s[j:i+j] == tmp:
+                #카운트 추가
+                cnt += 1
+            #반복 아니면
             else:
-                if cnt!=1:
-                    b = b+ str(cnt) + tmp
+                #반복된 문자열이 있으면
+                if cnt != 1:
+                    sentence = sentence + str(cnt) + tmp
                 else:
-                    b = b+tmp
-                tmp = s[j:j+i]
+                    sentence = sentence + tmp
+                #같지 않으니 tmp를 바꿔주고 cnt도 초기화 해준다.
+                tmp = s[j:i+j]
                 cnt = 1
-        result.append(len(b))
+        result.append(len(sentence))
+    #이제 결과 값에 문장들이 모일것이다. 이중 가장 짧은 길이를 답으로 출력하면 된다.
+        
     return min(result)
+

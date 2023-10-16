2b7c035804dca1f407e2cacc9dc969ace128e35b
yencoding <79498424+yeni28@users.noreply.github.com>
2023-10-16 22:07:36 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 31748 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,53 +1,50 @@
 import sys
-sys.setrecursionlimit(10**6) # 파이썬의 재귀 깊이 지정 (Python3)
+sys.setrecursionlimit(10 ** 6)  # 파이썬의 재귀 깊이 지정 (Python3)
 input = sys.stdin.readline
 N = int(input())
 
-graph = []
+area = []
 visited = [[0] * (N) for _ in range(N)]
 sum = 0
 sum2 = 0
 
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+# 상하좌우
+di = [-1, 1, 0, 0]
+dj = [0, 0, -1, 1]
 
 for i in range(N):
-    graph.append(list(input()))
+    area.append(list(input()))
 
-def dfs(x, y, color):
-    visited[x][y] = 1
+def dfs(i, j, color):
+    visited[i][j] = 1
 
-    # 동서남북 탐색
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        # 범위 내에 있고 방문한 적이 없다면
-        if 0 <= nx <= N-1 and 0 <= ny <= N-1 and visited[nx][ny] == 0:
-            # 위 조건을 모두 만족하면서 탐색중인 색상과 같은 색상이면 탐색
-            if graph[nx][ny] == color:
-                dfs(nx, ny, color)
+    for d in range(4):
+        ni = i + di[d]
+        nj = j + dj[d]
+        if 0 <= ni < N and 0 <= nj < N and visited[ni][nj] == 0:
+            if area[ni][nj] == color:
+                dfs(ni, nj, color)
 
-for color in ['R','G','B']: # 빨->초->파 순서로 확인
+
+for color in ['R', 'G', 'B']:
     for i in range(N):
         for j in range(N):
-            if visited[i][j] == 0 and graph[i][j] == color:
-                dfs(i, j,color)
+            if visited[i][j] == 0 and area[i][j] == color:
+                dfs(i, j, color)
                 sum += 1
 
-# 기존 그래프에서 초록색 -> 빨간색으로 변경
 for i in range(N):
     for j in range(N):
-        if graph[i][j] == 'G':
-            graph[i][j] = 'R'
+        if area[i][j] == 'G':
+            area[i][j] = 'R'
 
-# 방문 정보 초기화
 visited = [[0] * (N) for _ in range(N)]
 
-for color in ['R', 'B']: # 빨,파만 확인
+for color in ['R', 'B']:
     for i in range(N):
         for j in range(N):
-            if visited[i][j] == 0 and graph[i][j] == color:
-                dfs(i, j,color)
+            if visited[i][j] == 0 and area[i][j] == color:
+                dfs(i, j, color)
                 sum2 += 1
 
 print(sum, sum2)

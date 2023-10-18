799db84b2c66c3cf834ff1f229faf31a1c771802
yencoding <79498424+yeni28@users.noreply.github.com>
2023-10-18 15:25:46 +0900

    [level 2] Title: 모음 사전, Time: 1.43 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,20 +1,15 @@
 def solution(word):
-    # 모음만 사용해 만들 수 있는 길이 최대 5이하 A=>UUUUU
-    # 사전에서 몇 번째 단어인지
     
-    answer = 0
-    words ='AEIOU'
-    # 단어를 만들어볼까요
     word_list = []
-    # 각 최대 5개씩 쓸 수 있다.
-    visited = [0] * (6)
-    def dfs(cnt,w ):
+    words = 'AEIOU'
+    
+    def dfs(cnt,w):
         if cnt == 5:
             return
-        for i in range(len(words)):
-            word_list.append(w+words[i])
-            dfs(cnt+1, w+words[i])
-    
+        else:
+            for i in range(5):
+                word_list.append(w + words[i])
+                dfs(cnt+1, w+words[i])
     dfs(0,'')
         
     

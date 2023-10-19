a3f88d5f37857916afd17bbcbd92f238b62059be
yencoding <79498424+yeni28@users.noreply.github.com>
2023-10-19 15:17:31 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,19 +1,18 @@
 def solution(citations):
-    answer = 0
-    #n 편중 h번이상 인용된 논문이 h번 이상, 나머지는 h번 이하
-    citations.sort()
-    N = len(citations)
-    # h 선택 , h이상이 h개 이상인가? 최댓값인가? 
-    maxv = max(citations) + 1
     
-    h_val = 0
-    for n in range(0, maxv):
-        cnt = 0
-        for write in range(N):
-            if citations[write] >= n:
-                cnt += 1
-        if cnt >= n:
-            h_val = max(n, h_val)
-            
+    # 정렬 해 놓기
+    citations.sort()
+    maxv = max(citations)
+    N = len(citations)
+    maxh = 0
+    for h in range(0,maxv):
+        cnth = 0
+        for i in range(N):
+            if citations[i] >= h:
+                cnth +=1
+        # 센 갯수가 h보다 크다면
+        if cnth >= h:
+            maxh = max(h, maxh)
+                
 
-    return h_val
+    return maxh

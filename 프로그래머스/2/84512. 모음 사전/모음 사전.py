b44a32c14ed9c718cde878d789f3e8e170bd53cf
yencoding <79498424+yeni28@users.noreply.github.com>
2023-10-19 22:32:00 +0900

    [level 2] Title: 모음 사전, Time: 1.44 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,16 +1,19 @@
 def solution(word):
     
-    word_list = []
     words = 'AEIOU'
+    word_list = []
     
-    def dfs(cnt,w):
+    def dfs(cnt, w):
         if cnt == 5:
             return
         else:
             for i in range(5):
-                word_list.append(w + words[i])
-                dfs(cnt+1, w+words[i])
+                word_list.append(w+words[i])
+                dfs(cnt+1 , w+words[i])
+    
     dfs(0,'')
-        
+    
+                
+    
     
     return word_list.index(word) + 1

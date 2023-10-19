f9a99e0c4b5082db1531cc30e15dd1f0b4e1aef1
yencoding <79498424+yeni28@users.noreply.github.com>
2023-10-19 22:34:35 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,5 @@
 def solution(numbers):
     s = list(map(str,numbers))
-    a = sorted(s, key=lambda x : x*3, reverse=True)
+    s = sorted(s , key=lambda x : x*3, reverse=True)
     
-    return str(int("".join(a)))
+    return str(int(''.join(s)))

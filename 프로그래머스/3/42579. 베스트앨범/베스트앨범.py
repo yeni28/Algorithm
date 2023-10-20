cbb2990a3c7fe18506fe8b0c5e6b241ba213d6a6
yencoding <79498424+yeni28@users.noreply.github.com>
2023-10-20 13:44:20 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,27 +1,37 @@
+from collections import defaultdict
+
 def solution(genres, plays):
     answer = []
-    temp = []
-    total_genre_d = {}
-
-    temp = [[genres[i], plays[i], i] for i in range(len(genres))]   # [장르, 재생횟수, 고유 번호] 리스트
-    temp = sorted(temp, key=lambda x: (x[0], -x[1], x[2]))  # 많이 재생될수록, 같다면 고유번호가 낮을수록
-
-    for g in temp:  # {장르 : 총 재생횟수} 딕셔너리 생성
-        if g[0] not in total_genre_d:
-            total_genre_d[g[0]] = g[1]
-        else:
-            total_genre_d[g[0]] += g[1]
+    N = len(genres)
+    musics = [[genres[i],plays[i],i] for i in range(N)]
+    sort_music = sorted(musics, key=lambda x: (x[0],-x[1],x[2]))
     
-    total_genre_d = sorted(total_genre_d.items(), key = lambda x: -x[1])    # 재생횟수가 많은 순서로 장르 정렬
+    # 장르별 개수 더해서 큰 순으로 정렬
+    gen = set(genres)
+    gen_sum = defaultdict(list)
+    for g in list(gen):
+        for i in range(N):
+            if g == musics[i][0]:
+                gen_sum[g].append(musics[i][1])
+    gen_sum = sorted(gen_sum.items(), key=lambda x : - sum(x[1]))
+    gen_list = []
+    for gen in gen_sum:
+        gen_list.append(gen[0])
     
-    for i in total_genre_d: # 같은 장르 내에서는 최대 2곡까지 조건대로 수록
-        count = 0
-        for j in temp:
-            if i[0] == j[0]:
-                count += 1
-                if count > 2:
-                    break
-                else:
-                    answer.append(j[2])
+    # 다시구하기
+    
+    for g in gen_list:
+        cnt = 1
+        for i in range(N):
+            if cnt > 2:
+                break
+            elif g == sort_music[i][0] and cnt <= 2:
+                answer.append(sort_music[i][2])
+                cnt += 1
+            
+    
 
+    
+    
+    
     return answer

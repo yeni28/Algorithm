60648ed1aa849468b47013c99e39520c1b822d31
yencoding <79498424+yeni28@users.noreply.github.com>
2023-10-21 11:27:03 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,6 @@
+# 입양간, 보호기간 긴, 두마리, 아이디와 이름, 보호기간 내림차순
+SELECT a.ANIMAL_ID, a.NAME
+FROM ANIMAL_INS a JOIN ANIMAL_OUTS b
+ON a.ANIMAL_ID = b.ANIMAL_ID
+ORDER BY (b.DATETIME - a.DATETIME) DESC
+LIMIT 2

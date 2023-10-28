174b8988c43434ffab69e60464dafef552fe1c6e
yencoding <79498424+yeni28@users.noreply.github.com>
2023-10-28 15:13:54 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,38 +1,41 @@
 from collections import deque
 
 def solution(n, wires):
+    answer = -1
     
-    #연결을 표현할 그래프를 만든다.
     graph = [[] for _ in range(n+1)]
     
-    for a,b in wires:
+    for a, b in wires:
         graph[a].append(b)
         graph[b].append(a)
-        
-    # 연결 노드 찾기
-    def bfs(start):
+    
+    def dfs(start):
         visited = [0] * (n+1)
         visited[start] = 1
         q = deque([start])
-        cnt = 1 
+        cnt = 1
         while q:
             s = q.popleft()
             for i in graph[s]:
                 if not visited[i]:
-                    visited[i] = 1
                     q.append(i)
-                    cnt +=1
+                    visited[i] = 1
+                    cnt += 1 
         return cnt
-
+                
+    
     res = n
     for a,b in wires:
         graph[a].remove(b)
         graph[b].remove(a)
         
-        res = min(abs(bfs(b)-bfs(a)),res)
+        res = min(abs(dfs(a)-dfs(b)), res)
         
         graph[a].append(b)
         graph[b].append(a)
-    
-    
+        
+        
+        
+        
+        
     return res

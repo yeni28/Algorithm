a9257d7fd0e2b6a35460026d3f2b7a74da058059
yencoding <79498424+yeni28@users.noreply.github.com>
2023-11-28 23:56:24 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,26 @@
+import math
+
+def solution(progresses, speeds):
+    ans = []
+    works = []
+    N = len(speeds)
+    for i in range(N):
+        works.append(100 - int(progresses[i]))
+    remains = []
+    for j in range(N):
+        remains.append(math.ceil(int(works[j]) / int(speeds[j])))
+    v = [0] * N
+
+    for r in range(N):
+        cnt = 0
+        for n in range(0,N):
+            # 다음 값이 나보다 작으면
+            if r + n < N:
+                if int(remains[r]) >= int(remains[r + n]) and v[r+n] == 0 :
+                    cnt += 1
+                    v[r+n] = 1
+                else:
+                    break
+        if cnt != 0:
+            ans.append(cnt)
+    return ans
